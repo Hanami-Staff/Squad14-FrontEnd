b005205f8a1bbd5510f00049ac4c749cb8db06fe
RabbitDeWitt <davidsocoelho@outlook.com>
2024-08-02 17:25:53 -0300

    feat: add updateComment

--- src/hooks/useComment.ts
+++ src/hooks/useComment.ts
@@ -8,20 +8,41 @@ const useComment = () => {
   const { setComments } = useAppContext()
   /* const { dispatch } = useDialogContext() */
 
-  const createComment = (postId: string, comment: Comment) => {
+  const createComment = async (postId: string, comment: Comment) => {
     api.post(`/comments`, comment)
       .then(() => console.log("Comentário adicionado com sucesso!!!"))
       .catch(err => console.error(err.response.data.message))
       .finally(() => getComments(postId))
   }
 
-  const getComments = (postId: string) => {
+  const getComments = async (postId: string) => {
     api.get(`/commentsByPost/${postId}`)
       .then(res => {
         const ordenedComments = sortDate(res.data)
         setComments(ordenedComments as Array<Comment>)
       })
       .catch(err => console.error(err.message.data.message))
+  }
+
+  const getCommentById = async (commentId: string): Promise<Comment | undefined> => {
+    try {
+      const res = await api.get(`/comments/${commentId}`)
+      const comment = res.data
+      return comment
+    } catch (err) {
+      return undefined
+    }
+  }
+
+  const updateComment = async (postId: string, commentId: string, comment: Comment) => {
+    api.put(`/comments/${commentId}`, comment)
+      .then(res => {
+
+      })
+      .catch(err => {
+
+      })
+      .finally(() => getComments(postId))
   }
 
   /*  const deleteComment = (commentId: string) => {
@@ -32,6 +53,8 @@ const useComment = () => {
   return {
     createComment,
     getComments,
+    getCommentById,
+    updateComment
     /* deleteComment */
   }
 }
