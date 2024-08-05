d63660c3682dee6c8388fbed0fc59078451b666e
RabbitDeWitt <davidsocoelho@outlook.com>
2024-08-05 14:34:15 -0300

    refactor: add reloadPosts in usePosts

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -5,11 +5,17 @@ import { Operations } from "@/types"
 import { Post } from "@/types/Post"
 import { sortDate } from "@/utils/dateUtils"
 import useComment from "./useComment"
+import { usePathname } from "next/navigation"
 
 const usePost = () => {
-  const { setPosts } = useAppContext()
+  const { setPosts, user } = useAppContext()
   const { dispatch } = useDialogContext()
   const { getComments } = useComment()
+  const pathname = usePathname()
+
+  const realoadPosts = async () => {
+    pathname === '/' ? getAllPosts() : getPostsByUser(user?.id!)
+  }
 
   const createPost = async (post: Post) => {
     api.post('/posts', post)
@@ -23,7 +29,7 @@ const usePost = () => {
         })
       })
       .catch(err => console.error(err.response.data.message))
-      .finally(() => getAllPosts())
+      .finally(() => realoadPosts())
   }
 
   const getAllPosts = async () => {
@@ -32,6 +38,15 @@ const usePost = () => {
         setPosts(sortDate(res.data) as Array<Post>)
       })
       .catch(err => console.error("ERRO"))
+  }
+
+  const getPostsByUser = async (userId: string) => {
+    api.get(`/postsByUser/${userId}`)
+      .then(res => {
+        const ordenedPosts = sortDate(res.data)
+        setPosts(ordenedPosts as Array<Post>)
+      })
+      .catch(err => console.error(err.response.data.message))
   }
 
   const getPostById = async (id: string): Promise<Post | undefined> => {
@@ -53,7 +68,7 @@ const usePost = () => {
       .catch(err => {
         dispatch({ type: Operations.TOAST, payload: { type: "ERROR", message: err.response.data.message } })
       })
-      .finally(() => getAllPosts())
+      .finally(() => realoadPosts())
   }
 
 
@@ -66,12 +81,13 @@ const usePost = () => {
       .catch(err => {
         dispatch({ type: Operations.TOAST, payload: { type: "ERROR", message: err.response.data.message } })
       })
-      .finally(() => getAllPosts())
+      .finally(() => realoadPosts())
   }
 
   return {
     createPost,
     getAllPosts,
+    getPostsByUser,
     getPostById,
     updatePost,
     deletePost
